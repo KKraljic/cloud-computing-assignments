7a4d31e1214cea93a87d04b42c5603c709a272e9
Kraljic <karlo.kraljic@hpe.com>
2016-04-25 17:23:38 +0200

    Deleted sudo permissions for each statement

--- wordpress/wordpress_deployment.sh
+++ wordpress/wordpress_deployment.sh
@@ -1,4 +1,3 @@
-sudo docker build --no-cache -t wordpress .
-
-sudo docker run -d -e MYSQL_PASS="password" --name db -p 9001:3306 mysql
-sudo docker run -d --link db:db -e DB_PASS="password" -p 9000:80 wordpress
+docker build --no-cache -t wordpress .
+docker run -d -e MYSQL_PASS="password" --name db -p 9001:3306 mysql
+docker run -d --link db:db -e DB_PASS="password" -p 9000:80 wordpress
