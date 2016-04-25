8e9f4263e9297f820292532e836cefcb0539f423
Kraljic <karlo.kraljic@hpe.com>
2016-04-25 17:31:52 +0200

    Fixed command to deploy the mysql DB.

--- wordpress/wordpress_deployment.sh
+++ wordpress/wordpress_deployment.sh
@@ -1,3 +1,3 @@
 docker build --no-cache -t wordpress .
-docker run -d -e MYSQL_PASS="password" --name db -p 9001:3306 mysql
+docker run -d -e MYSQL_PASS="password" --name db -p 3306:3306 tutum/mysql:5.5
 docker run -d --link db:db -e DB_PASS="password" -p 9000:80 wordpress
