9f47ac37c9274ed28c77ec4e3abe542d237fb313
Kraljic <karlo.kraljic@hpe.com>
2016-04-25 23:05:10 +0200

    Finalized code and documentation. Ready for review.

--- wordpress/wordpress_deployment.sh
+++ wordpress/wordpress_deployment.sh
@@ -1,3 +1,3 @@
 docker build --no-cache -t wordpress .
 docker run -d -e MYSQL_PASS="password" --name db -p 3306:3306 tutum/mysql:5.5
-docker run -d --link db:db -e DB_PASS="password" -p 9000:80 wordpress
+docker run -d --link db:db -e DB_PASS="password" -p 9004:80 wordpress
